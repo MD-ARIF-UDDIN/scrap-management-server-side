30cecc1d6002b29ebfd75fb0335dd28a54b4d0c5
MD-ARIF-UDDIN <mdarifuddin040@gmail.comm>
2022-05-26 20:28:45 +0600

    added more

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const {
   MongoClient,
@@ -21,12 +22,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "dont have access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
   try {
     await client.connect();
     const toolCollection = client.db("scrap_tools_ltd").collection("tools");
-    const purchaseCollection = client.db("scrap_tools_ltd").collection("purchases");
+    const userCollection = client.db("scrap_tools_ltd").collection("users");
+    const reviewCollection = client.db("scrap_tools_ltd").collection("reviews");
+    const purchaseCollection = client
+      .db("scrap_tools_ltd")
+      .collection("purchases");
 
     app.get("/tool", async (req, res) => {
       const query = {};
@@ -40,29 +58,99 @@ async function run() {
       const tool = await toolCollection.findOne(query);
       res.send(tool);
     });
-    app.get("/purchase", async (req, res) => {
-      const query = {};
-      const cursor = purchaseCollection.find(query);
-      const tools = await cursor.toArray();
-      res.send(tools);
+
+
+    app.get('/user',verifyJWT,async(req,res)=>{
+      const users=await userCollection.find().toArray();
+      res.send(users);
+    })
+
+   app.get('/admin/:email',async(req,res)=>{
+     const email=req.params.email;
+     const user=await userCollection.findOne({email:email});
+     const isAdmin=user.role==='admin';
+     res.send({admin:isAdmin});
+   })
+
+
+    app.put("/user/admin/:email",verifyJWT, async (req, res) => {
+      const requester=req.decoded.email;
+      const requesterAccount=await userCollection.findOne({email:requester});
+      if(requesterAccount.role==='admin'){
+        const email = req.params.email;
+        const filter = { email: email };
+        const updateDoc = {
+          $set: {role:'admin'},
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message:'forbidden'});
+      }    
     });
-    // app.delete("/purchase/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await purchaseCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-    app.get("/purchase", async (req, res) => {
+
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.delete("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/purchase", verifyJWT, async (req, res) => {
       const customer = req.query.customer;
-      const query = { customer: customer};
-      const cursor = purchaseCollection.find(query);
-      const tools = await cursor.toArray();
-      res.send(tools);
+      const decodedEmail = req.decoded.email;
+      if (customer === decodedEmail) {
+        const query = { customer: customer };
+        const purchases = await purchaseCollection.find(query).toArray();
+        res.send(purchases);
+      }
+      else{
+        return res.status(403).send({message:'forbidden access'});
+      }
+    });
+
+    app.get("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await purchaseCollection.findOne(query);
+      res.send(tool);
     });
 
     app.post("/purchase", async (req, res) => {
-      const purchase= req.body;
+      const purchase = req.body;
       const result = await purchaseCollection.insertOne(purchase);
+      res.send(result);
+    });
+
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
 
@@ -72,14 +160,9 @@ async function run() {
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = { $set: updateStock };
-      const result = await toolCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
+      const result = await toolCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
-
   } finally {
   }
 }
